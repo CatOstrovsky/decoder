20bc82e178802c659dc84c61f11a2d0922c1e32c
Konstantin Ostrovsky <k.ostrovsky@xsquare.ru>
2023-01-09 16:24:19 +0300

    Prod config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/demo/decoder/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
